394274192cbd3e21fc3b3bbde977d580174d955e
alsdk1676 <alsdk6761@gmail.com>
2025-04-21 14:15:48 +0900

    v1

---
+++ src/main/java/com/app/gradationback/GradationBackApplication.java
@@ -0,0 +1,13 @@
+package com.app.gradationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradationBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradationBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/gradationback/GradationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.gradationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
