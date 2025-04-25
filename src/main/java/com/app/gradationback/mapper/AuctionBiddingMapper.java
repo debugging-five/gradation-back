99cefa5593b6738e6eb5555600e1f2c37a40eac3
sehyunf <ssam010925@gmail.com>
2025-04-25 11:10:02 +0900

    build: AuctionBiggingDAO, AuctionBiddingMapper 생성, 기존의 auction에서 분리

---
+++ src/main/java/com/app/gradationback/mapper/AuctionBiddingMapper.java
@@ -0,0 +1,9 @@
+package com.app.gradationback.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface AuctionBiddingMapper {
+
+}
---
+++ src/main/java/com/app/gradationback/repository/AuctionBiddingDAO.java
@@ -0,0 +1,11 @@
+package com.app.gradationback.repository;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class AuctionBiddingDAO {
+
+}
