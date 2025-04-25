1ae1a2b344178a56ba883afaae6a65f7c8d19ccb
croissen <croissen214@gmail.com>
2025-04-25 17:17:05 +0900

    feat: mailMapper 수정중

--- src/test/java/com/app/gradationback/mapper/seungmin/MailMapperTests.java
+++ src/test/java/com/app/gradationback/mapper/seungmin/MailMapperTests.java
@@ -7,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 @Slf4j
@@ -31,7 +34,7 @@ public class MailMapperTests {
     public void selectReceivedTest() {
         List<MailDTO> receivedList = mailMapper.selectReceived(4L);
         for (MailDTO mail : receivedList) {
-            log.info("메일 제목: {}, 보낸 사람 닉네임: {}, 보낸 시간: {}",
+            log.info("메일 제목: {}, 발신인: {}, 작성일: {}",
                     mail.getMailTitle(),
                     mail.getSendUserNickName(),
                     mail.getMailSendTime());
@@ -42,12 +45,49 @@ public class MailMapperTests {
     public void selectSendedTest() {
         List<MailDTO> sendedList = mailMapper.selectSended(2L);
         for (MailDTO mail : sendedList) {
-            log.info("메일 제목: {}, 수신인 닉네임: {}, 보낸 시간: {}",
+            log.info("제목: {}, 수신인: {}, 작성일: {}",
                     mail.getMailTitle(),
                     mail.getReceiveUserNickName(),
                     mail.getMailSendTime());
         }
     }
+
+//    받은 쪽지
+    @Test
+    public void selectReceivedDetailTest() {
+        Optional<MailDTO> Mail = mailMapper.selectReceivedDetail(8L, 5L);
+
+        if (Mail.isPresent()) {
+            MailDTO mail = Mail.get();
+            log.info(": {}", mail.getMailTitle());
+            log.info(": {}", mail.getMailContent());
+            log.info("발신자: {}", mail.getSendUserNickName());
+            log.info("메일주소: {}", mail.getSendUserEmail());
+            log.info(": {}", mail.getMailSendTime());
+        } else {
+            log.info("해당 쪽지를 찾을 수 없습니다.");
+        }
+    }
+
+//    내가 보낸쪽지
+    @Test
+    public void selectSendedDetailTest() {
+        Optional<MailDTO> Mail = mailMapper.selectSendedDetail(3L, 2L);
+
+        if (Mail.isPresent()) {
+            MailDTO mail = Mail.get();
+            log.info(": {}", mail.getMailTitle());
+            log.info(": {}", mail.getMailContent());
+            log.info("수신자: {}", mail.getReceiveUserNickName());
+            log.info("메일주소: {}", mail.getReceiveUserEmail());
+            log.info(": {}", mail.getMailSendTime());
+        } else {
+            log.info("해당 쪽지를 찾을 수 없습니다.");
+        }
+    }
+
+
+
 
 
 }
