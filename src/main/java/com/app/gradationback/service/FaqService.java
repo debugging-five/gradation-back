211692403a89e79b9cfad61496fbc8900512a02a
croissen <croissen214@gmail.com>
2025-04-29 14:13:12 +0900

    feat: faq 백완료

--- src/main/java/com/app/gradationback/service/FaqService.java
+++ src/main/java/com/app/gradationback/service/FaqService.java
@@ -9,13 +9,13 @@ import java.util.Optional;
 public interface FaqService {
 
 //    자주 묻는 질문 등록
-    public void registraction(FaqVO faqVO);
+    public void register(FaqVO faqVO);
 
 //    단일 조회
-    public Optional<FaqVO> getFaq(Long id);
+    public Optional<FaqVO> findByFaq(Long id);
 
 //    전체 조회
-    public List<FaqVO> getFaqList();
+    public List<FaqVO> getFaqList(FaqDTO faqDTO);
 
 //    수정
     public void modify(FaqVO faqVO);
