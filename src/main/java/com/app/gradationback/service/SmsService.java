ce88f0bf52ab31d22c437627f6af74c382431278
alsdk1676 <alsdk6761@gmail.com>
2025-05-07 16:17:04 +0900

    feat: userMapper.xml 수정

--- src/main/java/com/app/gradationback/service/SmsService.java
+++ src/main/java/com/app/gradationback/service/SmsService.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public interface SmsService {
 
 //    이메일 전송
-    public ResponseEntity<Map<String, Object>> transferMessage(String memberPhone);
+    public ResponseEntity<Map<String, Object>> sendEmailVerification(String memberEmail);
 
 //    인증코드 확인
     public boolean verifyAuthCode(String authCode);
