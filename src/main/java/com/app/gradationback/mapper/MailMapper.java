6f07c9087112c85744779d738e609b81b6718bc7
croissen <croissen214@gmail.com>
2025-05-09 20:27:56 +0900

    feat: Mail매퍼수정중

--- src/main/java/com/app/gradationback/mapper/MailMapper.java
+++ src/main/java/com/app/gradationback/mapper/MailMapper.java
@@ -27,9 +27,20 @@ public interface MailMapper {
     public Optional<MailDTO> selectSendedDetail(Long id, Long sendUserId);
 
 //    내가 보낸 쪽지 삭제
-    public Optional<MailDTO> deleteSendedMail(Long id , Long sendUserId);
+    public void deleteSendedMail(Long id , Long sendUserId);
 
 //    받은 쪽지 삭제
-    public Optional<MailDTO> deleteReceivedMail(Long id, Long receiveUserId);
+    public void deleteReceivedMail(Long id, Long receiveUserId);
 
+//    알림리스트
+    public List<MailDTO> selectAlert(Long receiveUserId);
+
+//    알림상세
+    public Optional<MailDTO> selectAlertDetail(Long id, Long receiveUserId);
+
+//    읽음처리
+    public void readUpdate (MailDTO mailDTO);
+
+//    읽지않은 메일 갯수 카운트
+    public int countNotRead(Long userId);
 }
