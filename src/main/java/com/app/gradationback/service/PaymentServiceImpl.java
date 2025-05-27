1328bd19627d37a84e9ea9f0725dd6ab86f4ebfa
sehyunf <ssam010925@gmail.com>
2025-05-27 12:06:26 +0900

    fix: payment 카드사 결제시 오류 수정

--- src/main/java/com/app/gradationback/service/PaymentServiceImpl.java
+++ src/main/java/com/app/gradationback/service/PaymentServiceImpl.java
@@ -39,6 +39,31 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public Optional<DeliveryDTO> payment(Map<String, Object> paymentData) {
+        Map<String, String> cardCompanyMap = new HashMap<>();
+        cardCompanyMap.put("11", "국민카드");
+        cardCompanyMap.put("21", "하나카드");
+        cardCompanyMap.put("31", "BC카드");
+        cardCompanyMap.put("3K", "기업BC카드");
+        cardCompanyMap.put("41", "신한카드");
+        cardCompanyMap.put("51", "삼성카드");
+        cardCompanyMap.put("61", "현대카드");
+        cardCompanyMap.put("71", "롯데카드");
+        cardCompanyMap.put("91", "NH농협카드");
+        cardCompanyMap.put("36", "씨티카드");
+        cardCompanyMap.put("33", "우리BC카드");
+        cardCompanyMap.put("W1", "우리카드"); // 응답 전용 코드
+        cardCompanyMap.put("15", "카카오뱅크카드");
+        cardCompanyMap.put("3A", "케이뱅크카드");
+        cardCompanyMap.put("24", "토스뱅크카드");
+        cardCompanyMap.put("46", "광주은행카드");
+        cardCompanyMap.put("35", "전북은행카드");
+        cardCompanyMap.put("42", "제주은행카드");
+        cardCompanyMap.put("30", "한국산업은행카드");
+        cardCompanyMap.put("34", "Sh수협은행카드");
+        cardCompanyMap.put("62", "신협카드");
+        cardCompanyMap.put("38", "새마을금고카드");
+        cardCompanyMap.put("37", "우체국예금보험카드");
+        cardCompanyMap.put("39", "저축은행중앙회카드");
 
         RestTemplate restTemplate = new RestTemplate();
         Long auctionId = Long.valueOf(paymentData.get("auctionId").toString());
@@ -62,12 +87,19 @@ public class PaymentServiceImpl implements PaymentService {
         DeliveryVO deliveryVO = new DeliveryVO();
         try {
             JsonNode responseNode = objectMapper.readTree(jsonString);
-            paymentVO.setPaymentMethod(responseNode.path("easyPay").path("provider").asText());
-            paymentVO.setPaymentAmount(responseNode.path("easyPay").path("amount").asInt());
-            paymentVO.setPaymentCode(responseNode.path("orderId").asText());
-            paymentVO.setAuctionId(auctionId);
-//            log.info("paymentVO: " + paymentVO);
-            paymentDAO.save(paymentVO);
+            if(responseNode.hasNonNull("card")) {
+                paymentVO.setPaymentMethod(cardCompanyMap.get(responseNode.path("card").path("issuerCode").asText()));
+                paymentVO.setPaymentAmount(responseNode.path("card").path("amount").asInt());
+                paymentVO.setPaymentCode(responseNode.path("orderId").asText());
+                paymentVO.setAuctionId(auctionId);
+                paymentDAO.save(paymentVO);
+            }else {
+                paymentVO.setPaymentMethod(responseNode.path("easyPay").path("provider").asText());
+                paymentVO.setPaymentAmount(responseNode.path("easyPay").path("amount").asInt());
+                paymentVO.setPaymentCode(responseNode.path("orderId").asText());
+                paymentVO.setAuctionId(auctionId);
+                paymentDAO.save(paymentVO);
+            }
 
             deliveryVO.setPaymentId(paymentVO.getId());
             deliveryVO.setDeliveryAddress(paymentData.get("deliveryAddress").toString());
@@ -76,8 +108,6 @@ public class PaymentServiceImpl implements PaymentService {
             deliveryVO.setDeliveryMessage(paymentData.get("deliveryMessage").toString());
             deliveryVO.setDeliveryReceiver(paymentData.get("deliveryReceiver").toString());
             deliveryVO.setDeliveryPhone(paymentData.get("deliveryPhone").toString());
-
-//            log.info("deliveryVO: " + deliveryVO);
 
             deliveryDAO.save(deliveryVO);
 
